dad58abb05aa96d76678d3bb78de7b7d8b2ccffc
sakrock <message.saket@gmail.com>
2020-05-05 22:51:54 +0530

    Version 1.0

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use(express.static('./public'));
 //fireControllers
 todoController(app);
 
-app.listen(3001);
+app.listen(3000);
 console.log("You are listening to port 3000");
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,30 +10,36 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo',todoSchema); 
-var itemOne= Todo({item:"Buy Flowers"}).save(function(err){
-    if(err) throw err;
-    console.log('Item saved');
-});
 
 
-var data=[{item:"get milk"},{item:'walk dog'},{item:'kick someone ass'}];
+
+var data=[{item:null}];
+
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     app.get('/todo',function(req,res){
-        res.render('todo',{todos:data});
+        //get data from mongodb and pass it to view
+        Todo.find({},function(err,data){
+            if(err) throw err;
+            res.render('todo',{todos:data});
+        });
     });
     
     app.post('/todo',urlencodedParser,function(req,res){
-        data.push(req.body);
-        res.json(data);
+        //get data from the view and add it to mongoDB
+        var newTodo=Todo(req.body).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item',function(req,res){
-        data=data.filter(function(todo){
-            return todo.item.replace(/ /g,'-') !== req.params.item;
+        //delete the requested item from mongoDB
+        Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+            if(err) throw err;
+            res.json(data);    
         });
-        res.json(data);
     });
 
 };
