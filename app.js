5b4af540f5227441d4a3006f0c2e1e2c70a806ad
sakrock <message.saket@gmail.com>
2020-05-05 21:11:26 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var todoController =require('./controllers/todoController.js')
+var app=express();
+
+//Set up Templating Engine
+app.set('view engine','ejs');
+
+//static Files
+app.use(express.static('./public'));
+
+//fireControllers
+todoController(app);
+
+app.listen(3001);
+console.log("You are listening to port 3000");
---
+++ controllers/todoController.js
@@ -0,0 +1,39 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb+srv://test:test@todo-sjf7x.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true });
+
+//Create a Schema.. This is like a bluePrint!
+var todoSchema = new mongoose.Schema({
+    item:String
+});
+
+var Todo = mongoose.model('Todo',todoSchema); 
+var itemOne= Todo({item:"Buy Flowers"}).save(function(err){
+    if(err) throw err;
+    console.log('Item saved');
+});
+
+
+var data=[{item:"get milk"},{item:'walk dog'},{item:'kick someone ass'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app){
+    app.get('/todo',function(req,res){
+        res.render('todo',{todos:data});
+    });
+    
+    app.post('/todo',urlencodedParser,function(req,res){
+        data.push(req.body);
+        res.json(data);
+    });
+
+    app.delete('/todo/:item',function(req,res){
+        data=data.filter(function(todo){
+            return todo.item.replace(/ /g,'-') !== req.params.item;
+        });
+        res.json(data);
+    });
+
+};
